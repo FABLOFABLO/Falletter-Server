7b81df8b6609892ee50d0f1de00bea7e1234dff9
rudwk <tlsruwk12345@gmail.com>
2025-06-12 09:12:18 +0900

    프로젝트 세팅

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './user/entities/user.entity';
+import { PostModule } from './post/post.module';
+import { AuthModule } from './auth/auth.module';
+require('dotenv').config();
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      port: 3306,
+      // host: process.env.DB_HOST,
+      // username: process.env.DB_NAME,
+      // password: process.env.DB_PWD,
+      // database: process.env.DB_DB,
+      host: 'localhost',
+      username: 'root',
+      password: 'user',
+      database: 'falletter',
+      entities: [UserEntity],
+      synchronize: true // 배포할때 주석처리하기
+    }),    
+    UserModule, PostModule, AuthModule,
+    /* the Module containing "UserEntityRepository" */],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
